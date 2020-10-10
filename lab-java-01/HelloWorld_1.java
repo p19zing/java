963209fe587044f6499ee1af4be192a5c41a7b27
p19zing <p19zing@ionio.gr>
2020-10-10 18:01:28 +0300

    //
    lab 1 / java

--- lab-java-01/HelloWorld_1.java
+++ lab-java-01/HelloWorld_1.java
@@ -1,15 +1,6 @@
-/******************************************************************************
- *  Τεκμηρίωση HelloWord προγράμματος
- *  Αρχείο πηγαίου κώδικα HelloWorld.java
- * 
- *  Πρώτο πρόγραμμα σε Java. Εμφανίζει το κείμενο Hello, World στο τερματικό.
- *  
- *  Compilation:  javac HelloWorld.java
- *  Execution:    java HelloWorld
- *
- ******************************************************************************/
 
-public class HelloWorld {
+
+public class HelloWorld_1 {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
